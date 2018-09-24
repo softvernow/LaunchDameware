7ffb5f400529448c1bd1bf186248971667b82f3c
Softvernow <38967263+softvernow@users.noreply.github.com>
2018-09-24 10:52:02 -0400

    Launch Dameware using C#
    On this Tutorial we will show you how to use Dameware to remote in to virtual or local machines using your C# .Net application.

---
+++ LaunchDameware/LaunchDameware/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace LaunchDameware
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDomain = new System.Windows.Forms.TextBox();
+            this.btnRDP = new System.Windows.Forms.Button();
+            this.txtMachineName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(87, 12);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.PasswordChar = '*';
+            this.txtUserName.Size = new System.Drawing.Size(165, 20);
+            this.txtUserName.TabIndex = 0;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(87, 38);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.PasswordChar = '*';
+            this.txtpassword.Size = new System.Drawing.Size(165, 20);
+            this.txtpassword.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "User Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Domain:";
+            // 
+            // txtDomain
+            // 
+            this.txtDomain.Location = new System.Drawing.Point(87, 64);
+            this.txtDomain.Name = "txtDomain";
+            this.txtDomain.PasswordChar = '*';
+            this.txtDomain.Size = new System.Drawing.Size(165, 20);
+            this.txtDomain.TabIndex = 5;
+            // 
+            // btnRDP
+            // 
+            this.btnRDP.Location = new System.Drawing.Point(15, 116);
+            this.btnRDP.Name = "btnRDP";
+            this.btnRDP.Size = new System.Drawing.Size(237, 23);
+            this.btnRDP.TabIndex = 6;
+            this.btnRDP.Text = "RDP";
+            this.btnRDP.UseVisualStyleBackColor = true;
+            this.btnRDP.Click += new System.EventHandler(this.btnRDP_Click);
+            // 
+            // txtMachineName
+            // 
+            this.txtMachineName.Location = new System.Drawing.Point(87, 90);
+            this.txtMachineName.Name = "txtMachineName";
+            this.txtMachineName.PasswordChar = '*';
+            this.txtMachineName.Size = new System.Drawing.Size(165, 20);
+            this.txtMachineName.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Machine:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 151);
+            this.Controls.Add(this.txtMachineName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnRDP);
+            this.Controls.Add(this.txtDomain);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtUserName);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDomain;
+        private System.Windows.Forms.Button btnRDP;
+        private System.Windows.Forms.TextBox txtMachineName;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ LaunchDameware/LaunchDameware/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaunchDameware
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void Open_Dameware()
+        {
+
+            String username = txtUserName.Text;
+            String password = txtpassword.Text;
+            String domain = txtDomain.Text;
+            String machine = txtMachineName.Text;
+
+            String dameware_file_exe = Get_Dameware_Exe();
+
+            try
+            {
+
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                System.Security.SecureString ssPwd = new System.Security.SecureString();
+                proc.StartInfo.UseShellExecute = true;
+                proc.StartInfo.FileName = dameware_file_exe;
+                proc.StartInfo.Arguments = " -c: -m:" + machine + " -u:" + username + " -p:" + password + " -d:" + domain +"";
+                proc.Start();
+
+            }
+            catch
+            {
+                MessageBox.Show("An Error Has Occured!");
+            }
+
+        }
+
+
+        private String Get_Dameware_Exe()
+        {
+            foreach (string file in Directory.EnumerateFiles(@"c:\program files", "DWRCC.exe", SearchOption.AllDirectories))
+            {
+                return file;
+            }
+
+            foreach (string file in Directory.EnumerateFiles(@"C:\Program Files (x86)", "DWRCC.exe", SearchOption.AllDirectories))
+            {
+                return file;
+            }
+
+            return String.Empty;
+        }
+
+        private void btnRDP_Click(object sender, EventArgs e)
+        {
+            Open_Dameware();
+        }
+    }
+}
